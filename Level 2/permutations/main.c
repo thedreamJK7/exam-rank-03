e87aae4214207cfec1f4391dd15eda704e45dfa2
Javokhir Kubaev <jkubaev7@gmail.com>
2025-10-06 18:43:11 +0200

    Permutation is fixed

--- Level 2/permutations/main.c
+++ Level 2/permutations/main.c
@@ -3,33 +3,54 @@
 #include <unistd.h>
 #include <string.h>
 
-void swap(char *x, char *y)
+void swap(char *str, int i, int j)
 {
 	char tmp;
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+	tmp = str[i];
+	str[i] = str[j];
+	str[j] = tmp;
 }
 
-void permute(char *str, int start_pos, int end_pos)
+int strsorted(char *str, int strlen) {
+	for (int i = 0; i < strlen - 1; i++) {
+		if (str[i + 1] < str[i])
+			return 0;
+	}
+	return 1;
+}
+
+void strsort(char *str, int strlen) {
+	for (int i = 0; i < strlen - 1; i++) {
+		for (int j = i + 1; j < strlen; j++) {
+			if (str[i] > str[j])
+				swap(str, i, j);
+			if (strsorted(str, strlen))
+				return;
+		}
+	}
+}
+
+void permute(char *str, int start_pos, int strlen)
 {
-	if (start_pos == end_pos)
+	if (start_pos == strlen )
 	{
 		puts(str);
 		return ;
 	}
-	for (int i = start_pos; i <= end_pos; i++)
+	for (int i = start_pos; i < strlen; i++)
 	{
-		swap((str + start_pos), (str + i));
-		permute(str, start_pos + 1, end_pos);
-		swap((str + start_pos), (str + i));
+		char *new = strdup(str);
+		swap(new, start_pos, i);
+		strsort(new + start_pos + 1, strlen - start_pos - 1);
+		permute(new, start_pos + 1, strlen);
+		free(new);
 	}
 }
 
 int main(void) 
 {
-	char str[] = "abc";
-    permute(str, 0, strlen("abc") - 1);
+	char str[] = "123";
+    permute(str, 0, strlen(str));
     return (0);
 }
 
